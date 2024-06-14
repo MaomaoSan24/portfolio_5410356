0207ce9fd88b9c6222b0e2d7a40839f26ff73ade
Monica Kenfack <monica.kenfack.tsamo@ei.thm.de>
2024-06-14 17:52:26 +0200

    Commented contact page out because i couldn´t fully edit it

--- portfolio_5410356/lib/profile_page.dart
+++ portfolio_5410356/lib/profile_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:portfolio_5410356/responsive_widget.dart';
+//import 'package:flutter_form_builder/flutter_form_builder.dart';
 
 //import 'package:portfolio_5410356/main.dart';
 // ignore: avoid_web_libraries_in_flutter
@@ -393,3 +394,79 @@ class SocialInfo extends StatelessWidget {
     );
   }
 }
+
+/*class ContactPage extends StatelessWidget {
+  const ContactPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text('Contact'),
+        backgroundColor: Colors.white,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(MediaQuery.of(context).size.height * 0.1),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              ContactFormWidget(),
+              // Add any additional contact information or content here
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ContactFormWidget extends StatelessWidget {
+  final GlobalKey<FormBuilderState> _formKey = GlobalKey<FormBuilderState>();
+
+  ContactFormWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FormBuilder(
+      key: _formKey,
+      child: Column(
+        children: [
+          FormBuilderTextField(
+            name: 'name',
+            decoration: const InputDecoration(labelText: 'Name'),
+            validator: FormBuilderValidators.required(context),
+          ),
+          FormBuilderTextField(
+            name: 'email',
+            decoration: const InputDecoration(labelText: 'Email'),
+            validator: FormBuilderValidators.email(context),
+          ),
+          FormBuilderTextField(
+            name: 'message',
+            decoration: const InputDecoration(labelText: 'Message'),
+            validator: FormBuilderValidators.required(context),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              if (_formKey.currentState!.saveAndValidate()) {
+                // Handle form submission
+                var formData = _formKey.currentState!.value;
+                // ignore: avoid_print
+                print(formData);
+              }
+            },
+            child: const Text('Submit'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class FormBuilderValidators {
+  static required(BuildContext context) {}
+  
+  static email(BuildContext context) {}
+}*/
